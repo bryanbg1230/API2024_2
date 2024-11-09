33a8b4e10e0813a8cd8b2fc001b7e1875b0af819
bryanbg1230 <archivosdepcguardados4@gmail.com>
2024-11-09 02:55:19 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -24,7 +24,9 @@ app.set('json spaces', 2); // Define la sangría para JSON en modo de desarrollo
 app.use(cors(corsOptions))
 app.use(express.json());//para que interprete los objetos json
 app.use(express.urlencoded({extended:true}));  //se añade para poder receptar formularios
-app.use('/uploads', express.static(path.resolve(__dirname, '../uploads')));
+//app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+//app.use('/uploads', express.static(path.resolve(__dirname, '../uploads')));
+app.use('/uploads', express.static(path.resolve('uploads')));
 //rutas
 app.use('/api',clientesRoutes)
 app.use('/api',usuariosRoutes)
