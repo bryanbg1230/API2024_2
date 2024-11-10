6512883aee6265146d0443d0c5b5391ba28bce68
bryanbg1230 <archivosdepcguardados4@gmail.com>
2024-11-10 02:36:10 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -10,8 +10,4 @@ export const PORT=process.env.PORT || 3000
 
 export const JWT_SECRET = process.env.JWT_SECRET || 'default_secret'; // Nueva variable para JWT
 
-export const CLOUDINARY_CLOUD_NAME=process.env.CLOUDINARY_CLOUD_NAME || 'ddvrhqgsm'
-export const CLOUDINARY_API_KEY=process.env.CLOUDINARY_API_KEY || '113551118694466'
-export const CLOUDINARY_API_SECRET=process.env.CLOUDINARY_API_SECRET || '4zk8XUMg2Fkc2Hk0qJMlbLtCZh4'
-
 console.log()
